34eebdc78a0c052b8f600a00b6376422ebed2769
Lacewell, Chaunte W <chaunte.w.lacewell@intel.com>
2025-05-06 15:29:47 -0700

    Add device tostart app script
    Signed-off-by: Lacewell, Chaunte W <chaunte.w.lacewell@intel.com>

--- start_app.sh
+++ start_app.sh
@@ -12,6 +12,7 @@ NSTREAMS=1
 IN_SOURCE=stream
 SOURCE="-DSTREAM_URL="rtp://127.0.0.1:8088" -DIN_SOURCE=${IN_SOURCE}"
 DEBUG="0"
+DEVICE="CPU"
 
 DIR=$(dirname $(readlink -f "$0"))
 BUILD_DIR=$DIR/build
@@ -25,12 +26,13 @@ LONG_LIST=(
     "ncpu"
     "source"
     "debug"
+    "device"
 )
 
 OPTS=$(getopt \
     --longoptions "$(printf "%s:," "${LONG_LIST[@]}")" \
     --name "$(basename "$0")" \
-    --options "hdi:t:r:n:v:c:s:" \
+    --options "hdi:t:r:n:v:c:s:e:" \
     -- "$@"
 )
 
@@ -53,13 +55,14 @@ script_usage()
 
     Options:
         -h                  optional    Print this help message
-        -i or --ingestion   optional    Ingestion type (object, face) [Default: "object,face"]
-        -t or --type        optional    Deployment method (compose) [Default: compose]
-        -r or --registry    optional    Registry [Default: None]
-        -n or --ncurations  optional    Number of ingestion containers [Default: 1]
-        -v or --nstreams    optional    Number of video streams [Default: 1]
-        -s or --source      optional    Input source type (videos, stream) [Default: stream]
         -d or --debug       optional    '1' to enable debug messages, otherwise '0'
+        -e or --device      optional    Device for inference (CPU, GPU) [Default: CPU]
+        -i or --ingestion   optional    Ingestion type (object, face) [Default: "object,face"]
+        -n or --ncurations  optional    Number of ingestion containers [Default: 1]
+        -r or --registry    optional    Registry [Default: None]
+        -s or --source      optional    Input source type (videos, stream) [Default: stream]
+        -t or --type        optional    Deployment method (compose) [Default: compose]
+        -v or --nstreams    optional    Number of video streams [Default: 1]
 
 EOF
 }
@@ -67,13 +70,12 @@ EOF
 while true; do
     case "$1" in
         -h) script_usage; exit 0 ;;
-        -d | --debug) shift; DEBUG="1" ;;
-        -i | --ingestion) shift; INGESTION=$1; shift ;;
-        -t | --type) shift; EXP_TYPE="$1"; shift ;;
-        -r | --registry) shift; REGISTRY="$1"; shift ;;
-        -n | --ncurations) shift; NCURATIONS=$1; shift ;;
-        -v | --nstreams) shift; NSTREAMS=$1; shift ;;
         -c | --ncpu) shift; NCPU=$1; shift ;;
+        -d | --debug) shift; DEBUG="1" ;;
+        -e | --device) shift; DEVICE=$1; shift ;;
+        -i | --ingestion) shift; INGESTION=$1; shift ;;
+        -n | --ncurations) shift; NCURATIONS=$1; shift ;;
+        -r | --registry) shift; REGISTRY="$1"; shift ;;
         -s | --source)
             shift;
             IN_SOURCE="$1";
@@ -92,6 +94,8 @@ while true; do
 
             shift;
             ;;
+        -t | --type) shift; EXP_TYPE="$1"; shift ;;
+        -v | --nstreams) shift; NSTREAMS=$1; shift ;;
         --) shift; break ;;
         *) script_usage; exit 0 ;;
     esac
@@ -102,9 +106,9 @@ done
 cd $BUILD_DIR
 
 if [ $REGISTRY == "None" ]; then
-    cmake -DINGESTION=$INGESTION $SOURCE -DNCPU=$NCPU -DNCURATIONS=$NCURATIONS -DNSTREAMS=$NSTREAMS -DDEBUG=$DEBUG ..
+    cmake -DINGESTION=$INGESTION $SOURCE -DNCPU=$NCPU -DNCURATIONS=$NCURATIONS -DNSTREAMS=$NSTREAMS -DDEBUG=$DEBUG -DDEVICE=$DEVICE ..
 else
-    cmake -DREGISTRY=$REGISTRY -DINGESTION=$INGESTION $SOURCE -DNCPU=$NCPU -DNCURATIONS=$NCURATIONS -DNSTREAMS=$NSTREAMS -DDEBUG=$DEBUG ..
+    cmake -DREGISTRY=$REGISTRY -DINGESTION=$INGESTION $SOURCE -DNCPU=$NCPU -DNCURATIONS=$NCURATIONS -DNSTREAMS=$NSTREAMS -DDEBUG=$DEBUG -DDEVICE=$DEVICE ..
 fi
 
 make
@@ -112,12 +116,12 @@ make
 if [ $EXP_TYPE == "compose" ]; then
     make start_docker_compose
 
-elif [ $EXP_TYPE == "k8" ]; then
-    if [ $REGISTRY == "None" ]; then
-        make update
-    fi
+# elif [ $EXP_TYPE == "k8" ]; then
+#     if [ $REGISTRY == "None" ]; then
+#         make update
+#     fi
 
-    make start_kubernetes
+#     make start_kubernetes
 
 else
     echo "INVALID TYPE: ${EXP_TYPE}"
