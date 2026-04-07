948e19bca79e5d0568878c84a4bbd6cfda16e590
Lacewell, Chaunte W <chaunte.w.lacewell@intel.com>
2026-04-06 19:02:12 -0700

    Pin pip as required by Scorecard / Pinned-Dependencies; Add scripts/files for generating hashes
    Signed-off-by: Lacewell, Chaunte W <chaunte.w.lacewell@intel.com>

--- .github/scripts/get_py_hashes.sh
+++ .github/scripts/get_py_hashes.sh
@@ -1,18 +1,18 @@
 # pip-compile is part of pip-tools pypi package
+# UV is required for hashes only and can be installed via `curl -LsSf https://astral.sh/uv/install.sh | sh`
+
+# Get absolute path of the script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+GH_DIR=$(dirname "${SCRIPT_DIR}")
+REPO_DIR=$(dirname "${GH_DIR}")
 
 # FRONTEND
-pip-compile -o frontend/requirements.txt --generate-hashes frontend/requirements.in
+pip-compile --no-header --no-annotate -o ${REPO_DIR}/frontend/requirements.txt --generate-hashes --allow-unsafe ${GH_DIR}/assets/frontend/requirements.in
 
 # UDF
-pip-compile -o udf/requirements.txt --generate-hashes udf/requirements.in
+pip-compile --no-header --no-annotate -o ${REPO_DIR}/udf/requirements.txt --generate-hashes --allow-unsafe ${GH_DIR}/assets/udf/requirements.in
 
 # VIDEO
-# curl -LsSf https://astral.sh/uv/install.sh | sh
-
-# pip-compile -o video/requirements.txt --generate-hashes video/requirements.in
-uv pip compile video/requirements.in -o video/requirements.txt --generate-hashes --index-strategy unsafe-best-match
-
-# pip-compile -o video/requirements.CPU.txt --generate-hashes video/requirements.CPU.in
-uv pip compile video/requirements.CPU.in -o video/requirements.CPU.txt --generate-hashes --index-strategy unsafe-best-match
-
-pip-compile -o video/requirements.GPU.txt --generate-hashes video/requirements.GPU.in
+uv pip compile ${GH_DIR}/assets/video/requirements.in --no-header --no-annotate -o ${REPO_DIR}/video/requirements.txt --generate-hashes --allow-unsafe --index-strategy unsafe-best-match
+uv pip compile ${GH_DIR}/assets/video/requirements.CPU.in --no-header --no-annotate -o ${REPO_DIR}/video/requirements.CPU.txt --generate-hashes --allow-unsafe --index-strategy unsafe-best-match
+pip-compile --no-header --no-annotate -o ${REPO_DIR}/video/requirements.GPU.txt --generate-hashes --allow-unsafe ${GH_DIR}/assets/video/requirements.GPU.in
