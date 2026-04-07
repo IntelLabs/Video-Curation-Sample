e47f37846968877e67286458b62a2b8251d82269
Lacewell, Chaunte W <chaunte.w.lacewell@intel.com>
2026-04-06 18:19:20 -0700

    corrected video requirements files and Dockerfile; Torch CPU version no longer pulling cuda
    Signed-off-by: Lacewell, Chaunte W <chaunte.w.lacewell@intel.com>

--- .github/scripts/get_py_hashes.sh
+++ .github/scripts/get_py_hashes.sh
@@ -7,10 +7,12 @@ pip-compile -o frontend/requirements.txt --generate-hashes frontend/requirements
 pip-compile -o udf/requirements.txt --generate-hashes udf/requirements.in
 
 # VIDEO
-pip-compile -o video/requirements.txt --generate-hashes video/requirements.in
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# pip-compile -o video/requirements.txt --generate-hashes video/requirements.in
+uv pip compile video/requirements.in -o video/requirements.txt --generate-hashes --index-strategy unsafe-best-match
 
 # pip-compile -o video/requirements.CPU.txt --generate-hashes video/requirements.CPU.in
-# curl -LsSf https://astral.sh/uv/install.sh | sh
-uv pip compile video/requirements.CPU.in -o video/requirements.CPU.txt --generate-hashes
+uv pip compile video/requirements.CPU.in -o video/requirements.CPU.txt --generate-hashes --index-strategy unsafe-best-match
 
 pip-compile -o video/requirements.GPU.txt --generate-hashes video/requirements.GPU.in
